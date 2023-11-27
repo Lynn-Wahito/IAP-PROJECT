094a6521f87d367b0c886f5789cc535161de36b3
Misati1 <misatisharly@gmail.com>
2023-11-27 10:33:42 +0300

    Set up customer dashboard

--- LaravelApp/resources/views/customer/customerIndex.blade.php
+++ LaravelApp/resources/views/customer/customerIndex.blade.php
@@ -16,6 +16,7 @@
                     <a href="#" class="bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-600 transition-colors duration-200">More Info</a>
                 </div>
             </div>
+</div>
 
 </x-customer-layout>
 
