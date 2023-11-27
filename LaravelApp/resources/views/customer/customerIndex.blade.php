be0517f4a0a60de944802381d9df22ca3bc93178
Misati1 <misatisharly@gmail.com>
2023-11-27 09:55:01 +0300

    Set up Customer Dashboard

---
+++ LaravelApp/resources/views/customer/customerIndex.blade.php
@@ -0,0 +1,21 @@
+<x-customer-layout>
+    <div class="w-full py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="w-full overflow-hidden bg-white shadow-xl sm:rounded-lg">
+                Customer Content
+            </div>
+         
+            <div class="container mx-auto px-4 py-16">
+        <h2 class="text-4xl font-bold mb-10 text-center">Event Posters</h2>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div class="bg-white rounded-lg overflow-hidden shadow-md transform transition-transform duration-500 hover:scale-105">
+                <img src="https://www.ticketsasa.com/components/com_enmasse/upload/Screenshot_2023-11-14_at_10.35.29.png1699947406.jpg" alt="Event Poster" class="w-full h-64 object-cover">
+                <div class="p-6">
+                    <h3 class="text-xl font-semibold mb-2">Tis The Season 2023 Show: A Very Kenyan Christmas Show!</h3>
+                    <p class="text-gray-500 mb-4">t's beginning to look a lot like Christmas...yup, it's just about time for 'Tis The Season!   This Year 'Tis The Season is a Festival with a Christmas pop up market, food, fun for the kids and individual performances by your TTS faves..the main show will start at 6:00pm!   Looking forward to celebrating A Very Kenyan Christmas with you.</p>
+                    <a href="#" class="bg-blue-500 text-white px-6 py-2 rounded-md hover:bg-blue-600 transition-colors duration-200">More Info</a>
+                </div>
+            </div>
+
+</x-customer-layout>
+
