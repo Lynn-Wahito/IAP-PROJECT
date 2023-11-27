521ad802a490003786cc9978b9d0f856cce2c3f2
Misati1 <misatisharly@gmail.com>
2023-11-27 09:30:04 +0300

    History of purchases for Customers

---
+++ LaravelApp/resources/views/customer/history.blade.php
@@ -0,0 +1,39 @@
+<x-customer-layout>
+
+
+    <div class="w-full py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                
+            <div class="w-full overflow-hidden bg-white shadow-xl sm:rounded-lg">
+               Event History
+            </div>
+            <div class="container mx-auto px-4 py-16">
+        <h2 class="text-4xl font-bold mb-10 text-center">Purchase History</h2>
+        <ul class="bg-white rounded-lg overflow-hidden shadow-md">
+            <li class="p-6">
+                <div class="grid grid-cols-4 gap-4">
+                    <div class="font-semibold">Abel Mutua Juice Party</div>
+                    <div class="col-span-3">The "Abel Mutua Juice Party Live Recording" was an engaging and lively event that featured Abel Mutua, a renowned figure in the entertainment industry. This live recording encompassed a mix of entertainment, humor, and insightful conversations.</div>
+                <div class="mt-4">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Read More
+                    </button>
+                </div>
+            </li>
+            <li class="border-t border-gray-200 p-6">
+                <div class="grid grid-cols-4 gap-4">
+                    <div class="font-semibold">Oktoba Fest</div>
+                    <div class="col-span-3">What started off as Kenya’s beer festival, has now expanded beyond borders to tap into Uganda, Tanzania, and South Sudan’s vibrant culture.   
+
+This year, OktobaFest East Africa edition italeta magic in the form of a celebration of East African culture. People from across all walks will come and see HOW WE DO beer, food, music, dance, fashion, art & games - at the biggest beer festival East Africa has experienced yet.  </div>
+                    <div class="mt-4">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Read More
+                    </button>
+                </div>
+                </li>
+    
+        </ul>
+    </div>
+</x-customer-layout>
+
